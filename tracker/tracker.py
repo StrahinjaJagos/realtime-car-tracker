d2a66ddd34ad0269cd0587255a9ce94d2507e1b1
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-14 13:28:57 +0100

    gps connection

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -1,12 +1,22 @@
-import geocoder
+import gps
 import time
 from firebase import update_location
 
+gpsd = gps.gps(mode=gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
 while True:
-    g = geocoder.ip('me')
-    latitude, longitude = g.latlng
-    print("Latitude:", latitude, "Longitude:", longitude)
-    
-    update_location(latitude, longitude)
+    try:
+        gps_data = gpsd.next()
+        
+        if gps_data['class'] == 'TPV':
+            if hasattr(gps_data, 'lat') and hasattr(gps_data, 'lon'):
+                latitude = gps_data.lat
+                longitude = gps_data.lon
+                print("Latitude:", latitude, "Longitude:", longitude)
+                
+                update_location(latitude, longitude)
+                
+    except Exception as e:
+        print("Error:", e)
     
     time.sleep(5)
